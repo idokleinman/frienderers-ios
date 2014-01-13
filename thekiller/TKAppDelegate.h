f9c8c47c4c8aa8cba4bc20dc3933e36d5bbf47d9
Elad Ben-Israel <elad.benisrael@gmail.com>
2014-01-13 17:42:30 +0100

    Initial Commit

---
+++ thekiller/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  thekiller
+//
+//  Created by Elad Ben-Israel on 1/13/14.
+//  Copyright (c) 2014 Citylifeapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thekillerTests/thekillerTests.m
@@ -0,0 +1,34 @@
+//
+//  thekillerTests.m
+//  thekillerTests
+//
+//  Created by Elad Ben-Israel on 1/13/14.
+//  Copyright (c) 2014 Citylifeapps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface thekillerTests : XCTestCase
+
+@end
+
+@implementation thekillerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
